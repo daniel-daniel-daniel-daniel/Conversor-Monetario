0a7daf52242f865b481de986049cf2d37b655a9b
Daniel <daniel.esteves.santana2006@gmail.com>
2026-04-17 20:24:40 -0300

    .

--- conversor.js
+++ conversor.js
@@ -1,53 +1,6 @@
-class moedaConverter {
-    static paraBRL(valorInput) {
-        return new Intl.NumberFormat('pt-BR', {
-            style: 'currency',
-            currency: 'BRL'
-        }).format(valorInput)}
-    
-    static paraUSD(valorInput) {
-        return new Intl.NumberFormat('en-US', {
-            style: 'currency',
-            currency: 'USD'
-        }).format(valorInput)}
+const moedas = [
+    { codigo: 'BRL', nome: 'Real' },
+    { codigo: 'USD', nome: 'Dólar' }
+]
 
-
-}
-
-
-
-function moedaCalcular() {
-
-const dados = {
-    valor: Number(document.getElementById("valorEntrada").value),
-    taxaCambio: 0
-}
-
-const moedaSelecionada = document.getElementById("moeda-select").value
-
-
-let formatar = ""
-let converterPara= ""
-
-//1 USD = 4,990 BRL
-//1 BRL = 0,20 USD
-switch(moedaSelecionada){
-    case "BRL":
-        dados.taxaCambio = 0.20
-        formatar=moedaConverter.paraBRL(dados.valor*4.99)
-        break;
-    case "USD":
-        dados.taxaCambio = 4.990
-        formatar=moedaConverter.paraUSD(dados.valor*0.20)
-        break;
-}
-
-//const converter = dados.valor * dados.taxaCambio
-
-
-
-
-const mensagemResultado = `Conversão: ${document.getElementById("moeda-select")} + formatar`
-
-document.getElementById("resultadoFinal").innerText = mensagemResultado
-}
+const optionsHTML = moedas.map(m => )
