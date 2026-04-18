33cce030957e04748eec61cbbf909bac60032fd9
daniel <daniel.esteves.santana2006@gmail.com>
2026-04-17 23:32:09 -0300

    progresso no seletor(código ainda não-funcional)

--- seletor.js
+++ seletor.js
@@ -1,20 +1,25 @@
-const moedas = [
+/*const moedas = [
     { codigo: 'BRL', nome: 'Real' },
     { codigo: 'USD', nome: 'Dólar' }
-]
+]*/
 
-function configurarSelects() {
+const moedas = await [fetch('moedas.json')]
+
+async function configurarSelects() {
     const origem = document.getElementById("origem")
     const destino = document.getElementById("destino")
 
-    const optionsHTML = moedas.map(m => `<option value=${m.codigo}>${m.nome}</option>`).join('\n');
+    const optionsHTML = moedas.map(m => `<option value="${m.codigo}">"${m.nome}"</option>`).join('\n');
+
+    console.log(optionsHTML)
     
     origem.innerHTML = optionsHTML;
     destino.innerHTML = optionsHTML;
 
     origem.value=moedas.at(0).codigo
-    origem.value=moedas.at(1).codigo
+    destino.value=moedas.at(1).codigo
+    
 }
 configurarSelects();
-
+console.log(moedas.at(0))
     
